9c336ca3eef8b9e087702a43c60edccfd8c9ebd3
Marcel Cornett <marcornett@gmail.com>
2020-09-10 19:57:35 -0400

    Implemented create parser function using argparse including description and arguments. Completed main function checking for namespace values with argument.

--- echo.py
+++ echo.py
@@ -2,22 +2,55 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility."""
 
-__author__ = "???"
+__author__ = "marcornett"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Returns an instance of argparse.ArgumentParser"""
-    # your code here
-    return
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text.')
+    parser.add_argument(
+        'text', help='text to be manipulated', nargs=1)
+    parser.add_argument(
+        '-u', '--upper', help='convert text to uppercase', action='store_true')
+    parser.add_argument(
+        '-l', '--lower', help='convert text to lowercase', action='store_true')
+    parser.add_argument(
+        '-t', '--title', help='convert text to titlecase', action='store_true')
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    # your code here
-    return
+
+    # arguments
+    parser = create_parser()
+    namespace = parser.parse_args(args)
+
+    if not namespace:
+        parser.print_usage()
+        sys.exit(1)
+    text = namespace.text[0]
+    lower = namespace.lower
+    title = namespace.title
+    upper = namespace.upper
+
+    output = text
+    if upper:
+        output = output.upper()
+        # print(text.upper())
+    if lower:
+        output = output.lower()
+        # print(text.lower())
+    if title:
+        output = output.title()
+        # print(text.title())
+
+    print(output)
 
 
 if __name__ == '__main__':
